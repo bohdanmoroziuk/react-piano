8845daa6cda89f577443e081dea4695fb3df1a37
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-04-15 10:00:54 +0300

    chore(domain): specify app constraints

--- src/domain/note.ts
+++ src/domain/note.ts
@@ -15,3 +15,40 @@ export type Note = {
   index: PitchIndex;
   octave: OctaveIndex;
 };
+
+const C1_MIDI_NUMBER = 24;
+
+const C4_MIDI_NUMBER = 60;
+
+const B5_MIDI_NUMBER = 83;
+
+export const LOWER_NOTE = C4_MIDI_NUMBER;
+
+export const HIGHER_NOTE = B5_MIDI_NUMBER;
+
+export const SEMITONES_IN_OCTAVE = 12;
+
+export const NATURAL_PITCH_INDICES: PitchIndex[] = [
+  0,
+  2,
+  4,
+  5,
+  7,
+  9,
+  11,
+];
+
+export const PITCHES_REGISTRY: Record<PitchIndex, NotePitch> = {
+  0: "C",
+  1: "C",
+  2: "D",
+  3: "D",
+  4: "E",
+  5: "F",
+  6: "F",
+  7: "G",
+  8: "G",
+  9: "A",
+  10: "A",
+  11: "B",
+};
