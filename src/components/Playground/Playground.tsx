799d34f666f5ca2ed7031264f72f0c1543f91030
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2022-04-18 11:47:31 +0300

    feat: update theme styles

--- src/components/Playground/Playground.tsx
+++ src/components/Playground/Playground.tsx
@@ -3,10 +3,12 @@ import { InstrumentProvider } from 'contexts/instrument';
 import KeyboardController from 'components/KeyboardController';
 import InstrumentSelector from 'components/InstrumentSelector';
 
+import styles from 'components/Playground/Playground.module.css';
+
 export default function Playground() {
   return (
     <InstrumentProvider>
-      <div className="playground">
+      <div className={styles.playground}>
         <KeyboardController />
         <InstrumentSelector />
       </div>
